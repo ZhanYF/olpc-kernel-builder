2ee95e3c3c3157db899b3fba0b544574ad416423
Martin Langhoff <martin@laptop.org>
2011-06-06 16:26:09 -0400

    Add support for ARM build, non-cjb build, dirty build

--- olpc-kernel-builder.sh
+++ olpc-kernel-builder.sh
@@ -7,12 +7,30 @@
 # of making small changes and compiling new kernels
 set -e
 set -x
-git_clone="/home/cjb/kernels/olpc-2.6"
-rpm_basedir="/home/cjb/kernels/rpms-out"
-syncdir="/home/cjb/kernels/rpms-sync"
-builddir="/home/cjb/kernels/rpm-build"
 
-build_configs="olpc-2.6.35,xo_1-kernel-rpm,f14-xo1 olpc-2.6.35,xo_1_5-kernel-rpm,f14-xo1.5"
+while [ "$1" != "" ]; do
+    case $1 in
+        -d | --dirty )          shift
+                                dirty=1
+                                ;;
+    *)                          break ;;
+    esac
+    
+done
+
+basedir=${1:-/home/cjb/kernels/}
+arch=`uname -m`
+
+git_clone="$basedir/olpc-2.6"
+rpm_basedir="$basedir/rpms-out"
+syncdir="$basedir/rpms-sync"
+builddir="$basedir/rpm-build"
+
+if [ "${arch:0:3}" == "arm" ]; then
+    build_configs="olpc-2.6.39,xo_1_75-kernel-rpm,f14-xo1.75"
+else
+    build_configs="olpc-2.6.35,xo_1-kernel-rpm,f14-xo1 olpc-2.6.35,xo_1_5-kernel-rpm,f14-xo1.5"
+fi
 
 datestamp=$(date +%Y%m%d%H%M%S)
 
@@ -38,8 +56,11 @@ for config in $build_configs; do
 	rpm_outdir=$tgtdir/build-$datestamp
 	rm -rf $rpm_outdir
 	mkdir -p $rpm_outdir
-	git checkout remotes/origin/$branch
+	if [ "$dirty" = "1" ]; then
+            git checkout remotes/origin/$branch
+        fi
 	git_head=$(git log -1 --format=%H)
+
 	[ -e $tgtdir/lastbuild ] && last_git_head=$(<$tgtdir/lastbuild)
 	if [[ $git_head == $last_git_head ]]; then
 		echo "Already built kernel $git_head"
